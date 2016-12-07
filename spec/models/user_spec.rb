3a32565b839291bf21cf9152a4a36858e458c00a
poguez <zeugop@gmail.com>
2016-12-07 13:22:58 +0900

    User basic tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe User do
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+
+  it { should be_valid }
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+end
